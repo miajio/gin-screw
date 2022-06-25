a3b707d7252eafc4ca9067e4fd902f814e6a3aa3
miajio <29160047@qq.com>
2022-06-26 02:08:30 +0800

    stringutil add md5 and aes en decrypt func

--- pkg/stringutil/root.go
+++ pkg/stringutil/root.go
@@ -1,5 +1,15 @@
 package stringutil
 
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/base64"
+	"fmt"
+)
+
 const (
 	NoBreakSp       = '\u00A0' // no-break space
 	NarrowNoBreakSp = '\u202F' // narrow no-break space
@@ -20,4 +30,52 @@ func DeSpace(val string) string {
 		result = append(result, v)
 	}
 	return string(result)
+}
+
+// Md5 return md5 string
+func Md5(val string) string {
+	return fmt.Sprintf("%x", md5.Sum([]byte(val)))
+}
+
+// Sha256 return sha256 string
+func Sha256(val string) string {
+	return fmt.Sprintf("%x", sha256.Sum256([]byte(val)))
+}
+
+// AesEncrypt return aes encrypt string
+func AesEncrypt(v, k string) (string, error) {
+	origData, key := []byte(v), []byte(k)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	blockSize := block.BlockSize()
+	padding := blockSize - len(origData)%blockSize
+	padtext := bytes.Repeat([]byte{byte(padding)}, padding)
+	origData = append(origData, padtext...)
+
+	blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
+	crypted := make([]byte, len(origData))
+	blockMode.CryptBlocks(crypted, origData)
+	return base64.StdEncoding.EncodeToString(crypted), nil
+}
+
+// AesDecrypt return aes decrypt string
+func AesDecrypt(v, k string) (string, error) {
+	crypted, err := base64.StdEncoding.DecodeString(v)
+	if err != nil {
+		return "", err
+	}
+	key := []byte(k)
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return "", err
+	}
+	blockSize := block.BlockSize()
+	blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
+	origData := make([]byte, len(crypted))
+	blockMode.CryptBlocks(origData, crypted)
+	length := len(origData)
+	unpadding := int(origData[length-1])
+	return string(origData[:(length - unpadding)]), nil
 }
